a2aa551e60332a571749558b6805b0bd6af0e6fb
txsniper <txawplh@126.com>
2017-06-19 15:12:54 +0800

    add deploy

---
+++ django/mysite/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+deploay_vps() {
+	BASE_DIR="/root/Django/mysite"
+	cp -R * $BASE_DIR/
+} 
+
+deploay_vps
--- django/mysite/mysite/views.py
+++ django/mysite/mysite/views.py
@@ -2,4 +2,5 @@ from django.http import HttpResponse
 def hello(request):
 	context = {}
 	context['hello'] = 'Hello World'
-	return HttpResponse("Hello World")
+	return render(request, 'hello.html',context)
+
